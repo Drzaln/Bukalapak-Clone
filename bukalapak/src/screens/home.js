ed7cace47dc17663d0ff55865b96f90c912170fe
Rezha <rezharatulangi@gmail.com>
2019-07-08 22:37:42 +0700

    carousel promo

--- bukalapak/src/screens/home.js
+++ bukalapak/src/screens/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../components/header/navbar';
 import Footer from '../components/footer/footer';
+import CarouselPromo from '../components/CarouselPromo/CarouselPromo';
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Header/>
+                <CarouselPromo/>
                 <h1>Home</h1>
                 <Footer/>
             </div>
