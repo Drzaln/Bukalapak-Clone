e883c29ac71feeee79069f6f4b4f4e0df9060a59
Drzaln <drzaln@null.net>
2019-07-08 21:08:53 +0700

    cicil list produk

--- bukalapak/src/index.js
+++ bukalapak/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-alice-carousel/lib/alice-carousel.css";
 
 ReactDOM.render(<BrowserRouter>
                     <App />   
